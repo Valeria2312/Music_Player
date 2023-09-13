413dda6bc374872e80d403dd29430da9e09baed7
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-09-13 17:32:06 +0300

    плеер

---
+++ src/js/data.js
@@ -0,0 +1,52 @@
+export const data = [
+    {
+        id: 1,
+        link: "Metallica_-_Enter_Sandman_47954409.mp3",
+        genre: "rock",
+        track: "Enter_Sandman",
+        group: "Metallica",
+    },
+    {
+        id: 2,
+        link: "Metallica_-_Fuel_47955068.mp3",
+        genre: "rock",
+        track: "Fuel",
+        group: "Metallica",
+    },
+    {
+        id: 3,
+        link: "Metallica_-_Nothing_Else_Matters_47954420.mp3",
+        genre: "rock",
+        track: "Nothing_Else_Matters",
+        group: "Metallica",
+    },
+    {
+        id: 4,
+        link: "Metallica_-_Seek_Destroy_47955765.mp3",
+        genre: "rock",
+        track: "Seek_Destroy",
+        group: "Metallica",
+    },
+    {
+        id: 5,
+        link: "Metallica_-_The_Unforgiven_47954414.mp3",
+        genre: "rock",
+        track: "The_Unforgiven",
+        group: "Metallica",
+    },
+    // {
+    //     id: 6,
+    //     link: "sample-12s.wav",
+    //     genre: "",
+    //     track: "sample-12s",
+    //     group: "Unknown performer",
+    // },
+    // {
+    //     id: 7,
+    //     link: "sample-15s.wav",
+    //     genre: "",
+    //     track: "sample-15s",
+    //     group: "Unknown performer",
+    // },
+];
+// export {data};
